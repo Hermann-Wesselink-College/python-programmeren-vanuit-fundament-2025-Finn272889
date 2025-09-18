a965066675b760dc99b871378eec209999a38a37
Finn272889 <37050@lln.hethwc.nl>
2025-09-18 09:43:36 +0200

    _6_Variabelen_1-5

--- 2_6_Variabelen_1-5.py
+++ 2_6_Variabelen_1-5.py
@@ -1,1 +1,17 @@
 # opdracht 1 tpt en met 5 van paragraaf 2.6
+postcode = "3343 PL"
+print(postcode.lower())
+
+woonplaats = Aalsmeer
+print = (woonplaats.upper())
+
+verzendmethode = "PostNL"
+print = (verzendmethode.lower())
+betaalmethode = "iDeal"
+print = (betaalmethode.upper())
+
+postcode  = input("Wat is je postcode?")
+print(postcode.upper())
+
+naam = Gasselternijveenschemond
+print = (len(naam))
